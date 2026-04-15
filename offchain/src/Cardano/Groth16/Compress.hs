f187795ae308e10ae0fac1f1d29e9ad7aaa2f8f1
paolino <paolo.veronelli@gmail.com>
2026-04-15 12:53:25 +0100

    feat: nixify project — flake checks for Rust FFI, circuit, lint, Aiken

--- offchain/src/Cardano/Groth16/Compress.hs
+++ offchain/src/Cardano/Groth16/Compress.hs
@@ -1,37 +1,37 @@
 -- | Compress snarkjs affine coordinates to BLS12-381 compressed form.
 module Cardano.Groth16.Compress (
-  compressProof
-, compressVK
+    compressProof,
+    compressVK,
 ) where
 
-import Data.ByteString (ByteString)
 import Cardano.Groth16.FFI (compressG1, compressG2)
 import Cardano.Groth16.Types (
-  CompressedProof (..)
-  , CompressedVK (..)
-  , G1Affine (..)
-  , G2Affine (..)
-  , SnarkjsProof (..)
-  , SnarkjsVK (..)
-  )
+    CompressedProof (..),
+    CompressedVK (..),
+    G1Affine (..),
+    G2Affine (..),
+    SnarkjsProof (..),
+    SnarkjsVK (..),
+ )
+import Data.ByteString (ByteString)
 
 -- | Compress all points in a snarkjs proof.
 compressProof :: SnarkjsProof -> IO CompressedProof
 compressProof p = do
-  a <- compressG1Point (proofA p)
-  b <- compressG2Point (proofB p)
-  c <- compressG1Point (proofC p)
-  pure (CompressedProof a b c)
+    a <- compressG1Point (proofA p)
+    b <- compressG2Point (proofB p)
+    c <- compressG1Point (proofC p)
+    pure (CompressedProof a b c)
 
 -- | Compress all points in a snarkjs verification key.
 compressVK :: SnarkjsVK -> IO CompressedVK
 compressVK vk = do
-  alpha <- compressG1Point (vkAlpha vk)
-  beta <- compressG2Point (vkBeta vk)
-  gamma <- compressG2Point (vkGamma vk)
-  delta <- compressG2Point (vkDelta vk)
-  ic <- traverse compressG1Point (vkIC vk)
-  pure (CompressedVK alpha beta gamma delta ic)
+    alpha <- compressG1Point (vkAlpha vk)
+    beta <- compressG2Point (vkBeta vk)
+    gamma <- compressG2Point (vkGamma vk)
+    delta <- compressG2Point (vkDelta vk)
+    ic <- traverse compressG1Point (vkIC vk)
+    pure (CompressedVK alpha beta gamma delta ic)
 
 compressG1Point :: G1Affine -> IO ByteString
 compressG1Point (G1Affine x y) = compressG1 x y
--- offchain/src/Cardano/Groth16/FFI.hs
+++ offchain/src/Cardano/Groth16/FFI.hs
@@ -1,7 +1,7 @@
 -- | FFI bindings for BLS12-381 point compression via blst.
 module Cardano.Groth16.FFI (
-  compressG1
-, compressG2
+    compressG1,
+    compressG2,
 ) where
 
 import Data.ByteString (ByteString)
@@ -13,86 +13,86 @@ import Foreign.Marshal.Alloc (allocaBytes)
 import Foreign.Ptr (Ptr, castPtr)
 
 foreign import ccall unsafe "groth16_g1_compress"
-  c_g1_compress
-    :: Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> IO CInt
+    c_g1_compress ::
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        IO CInt
 
 foreign import ccall unsafe "groth16_g2_compress"
-  c_g2_compress
-    :: Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> Ptr Word8
-    -> CSize
-    -> IO CInt
+    c_g2_compress ::
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        Ptr Word8 ->
+        CSize ->
+        IO CInt
 
 -- | Convert an arbitrary-precision integer to exactly 48 bytes big-endian.
 integerToBE48 :: Integer -> ByteString
 integerToBE48 n =
-  let raw = intToBytes (abs n)
-      padded = BS.replicate (48 - BS.length raw) 0 <> raw
-   in BS.take 48 padded
- where
-  intToBytes :: Integer -> ByteString
-  intToBytes 0 = BS.empty
-  intToBytes i =
-    let (q, r) = i `divMod` 256
-     in intToBytes q <> BS.singleton (fromIntegral r)
+    let raw = intToBytes (abs n)
+        padded = BS.replicate (48 - BS.length raw) 0 <> raw
+     in BS.take 48 padded
+  where
+    intToBytes :: Integer -> ByteString
+    intToBytes 0 = BS.empty
+    intToBytes i =
+        let (q, r) = i `divMod` 256
+         in intToBytes q <> BS.singleton (fromIntegral r)
 
 -- | Compress a G1 affine point (x, y as Integers) to 48 compressed bytes.
 compressG1 :: Integer -> Integer -> IO ByteString
 compressG1 x y = do
-  let xBE = integerToBE48 x
-      yBE = integerToBE48 y
-  BU.unsafeUseAsCStringLen xBE $ \(xPtr, _) ->
-    BU.unsafeUseAsCStringLen yBE $ \(yPtr, _) ->
-      allocaBytes 48 $ \outPtr -> do
-        rc <-
-          c_g1_compress
-            (castPtr xPtr)
-            48
-            (castPtr yPtr)
-            48
-            outPtr
-            48
-        if rc == 0
-          then BS.packCStringLen (castPtr outPtr, 48)
-          else error "groth16_g1_compress failed"
+    let xBE = integerToBE48 x
+        yBE = integerToBE48 y
+    BU.unsafeUseAsCStringLen xBE $ \(xPtr, _) ->
+        BU.unsafeUseAsCStringLen yBE $ \(yPtr, _) ->
+            allocaBytes 48 $ \outPtr -> do
+                rc <-
+                    c_g1_compress
+                        (castPtr xPtr)
+                        48
+                        (castPtr yPtr)
+                        48
+                        outPtr
+                        48
+                if rc == 0
+                    then BS.packCStringLen (castPtr outPtr, 48)
+                    else error "groth16_g1_compress failed"
 
 -- | Compress a G2 affine point (x0, x1, y0, y1 as Integers) to 96 compressed bytes.
 compressG2 :: Integer -> Integer -> Integer -> Integer -> IO ByteString
 compressG2 x0 x1 y0 y1 = do
-  let x0BE = integerToBE48 x0
-      x1BE = integerToBE48 x1
-      y0BE = integerToBE48 y0
-      y1BE = integerToBE48 y1
-  BU.unsafeUseAsCStringLen x0BE $ \(x0Ptr, _) ->
-    BU.unsafeUseAsCStringLen x1BE $ \(x1Ptr, _) ->
-      BU.unsafeUseAsCStringLen y0BE $ \(y0Ptr, _) ->
-        BU.unsafeUseAsCStringLen y1BE $ \(y1Ptr, _) ->
-          allocaBytes 96 $ \outPtr -> do
-            rc <-
-              c_g2_compress
-                (castPtr x0Ptr)
-                48
-                (castPtr x1Ptr)
-                48
-                (castPtr y0Ptr)
-                48
-                (castPtr y1Ptr)
-                48
-                outPtr
-                96
-            if rc == 0
-              then BS.packCStringLen (castPtr outPtr, 96)
-              else error "groth16_g2_compress failed"
+    let x0BE = integerToBE48 x0
+        x1BE = integerToBE48 x1
+        y0BE = integerToBE48 y0
+        y1BE = integerToBE48 y1
+    BU.unsafeUseAsCStringLen x0BE $ \(x0Ptr, _) ->
+        BU.unsafeUseAsCStringLen x1BE $ \(x1Ptr, _) ->
+            BU.unsafeUseAsCStringLen y0BE $ \(y0Ptr, _) ->
+                BU.unsafeUseAsCStringLen y1BE $ \(y1Ptr, _) ->
+                    allocaBytes 96 $ \outPtr -> do
+                        rc <-
+                            c_g2_compress
+                                (castPtr x0Ptr)
+                                48
+                                (castPtr x1Ptr)
+                                48
+                                (castPtr y0Ptr)
+                                48
+                                (castPtr y1Ptr)
+                                48
+                                outPtr
+                                96
+                        if rc == 0
+                            then BS.packCStringLen (castPtr outPtr, 96)
+                            else error "groth16_g2_compress failed"
